e412afb1a5f02958d779327084d9f9ad220df734
Louis <poostmaan@gmail.com>
2023-10-08 17:11:21 -0400

    router base added

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import "dotenv/config";
+import { router } from './routes';
 
 const app = express();
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(morgan('dev'));
 
-app.listen(() => {
-    console.log("running")
-}, port);
+app.use(router);
+
+app.listen(PORT, () => {
+    console.log(`running on port ${PORT}`)
+});
