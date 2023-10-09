e59da72112148d4acd4453fae0a5884c150cbae1
Louis <poostmaan@gmail.com>
2023-10-09 17:03:37 -0400

    Car interface

---
+++ src/interfaces/car.interface.ts
@@ -0,0 +1,10 @@
+export interface Car {
+    make: string
+    model: string
+    serial_numer: number
+    color: string
+    gas: "gasoline"| 'electric'
+    year: number
+    description: string
+    price: number
+}
